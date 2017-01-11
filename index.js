b7edef820b8f1816b80578019bdf6e3cbd5acf3a
Jawad Bangash <jawadbanga@gmail.com>
2017-01-11 05:02:58 +0000

    Done.

--- index.js
+++ index.js
@@ -14,10 +14,24 @@ function getRepositories() {
   req.open("GET", 'https://api.github.com/users/Jawadban/repos')
   req.send()
 }
+function getCommits (elem) {
+  const req = new XMLHttpRequest()
+  var name = elem.dataset.repo
+  req.addEventListener('load', showCommits)
+  req.open("GET", 'https://api.github.com/repos/Jawadban/' + name + '/commits')
+  req.send()
+}
+
+function showCommits(event, data) {
+  var commits = JSON.parse(this.responseText)
+  console.log(commits)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = commitsList;
+}
 
 function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
+  var repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
