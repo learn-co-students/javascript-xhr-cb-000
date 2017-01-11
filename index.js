65276270a74e54535476431822e8b824a37d66a3
Jawad Bangash <jawadbanga@gmail.com>
2017-01-11 03:55:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+// function getRepositories() {
+//   var req = new XMLHttpRequest();
+//   req.open("GET", 'https://api.github.com/users/Jawadban/repos');
+//   req.send();
+// }
+function showRepositories(event, data) {
+  //this is set to the XMLHttpRequest object that fired the event
+  console.log(this.responseText)
+}
+
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/Jawadban/repos')
+  req.send()
+}
+
+function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
